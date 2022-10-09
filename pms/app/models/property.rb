1c2d38ec6faa6523387f703ed79e1deff761b0d7
JosephShin <joespam9999@gmail.com>
2022-10-09 11:43:50 -0400

    added validations for apartments and properties on creation

--- pms/app/models/property.rb
+++ pms/app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
     has_many :apartments, through: :property_apartments
     has_many :owners, through: :owner_properties
+
+    validates :address, presence: true
+    validates :bedroomCount, presence: true, numericality: {only_integer: true}
+    validates :bathroomCount, presence: true, numericality: true
+    validates :propertySize, presence: true, numericality: {only_integer: true}
 end
